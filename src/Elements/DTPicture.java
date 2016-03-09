15f23a0159a6287bc1ecb092ac734d4d5adb5b68
uiynyny <y668zhan@uwaterloo.ca>
2016-03-09 12:55:15 -0500

    start on DTPicture

--- src/Elements/DTPicture.java
+++ src/Elements/DTPicture.java
@@ -4,12 +4,11 @@ import model.ImageModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.util.Observable;
 import java.util.Observer;
 
-/**
- * Created by temp user on 09/03/16.
- */
 public class DTPicture extends JComponent implements Observer {
     private ImageModel im;
     private Image image;
@@ -17,12 +16,40 @@ public class DTPicture extends JComponent implements Observer {
     public DTPicture(ImageModel im){
         this.im = im;
         image = new ImageIcon(im.getPath()).getImage();
-        layoutComponent();
+
+        setFocusable(true);
+        setPreferredSize(new Dimension(80,60));
+
+        controller();
         im.addObserver(this);
     }
 
-    private void layoutComponent(){
+    private void controller(){
+        addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+               im.updateView();
+            }
 
+            @Override
+            public void focusLost(FocusEvent e) {
+                im.updateView();
+            }
+        });
+    }
+
+    protected void paintComponent(Graphics g){
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, image.getWidth(this),image.getHeight(this));
+        g.drawImage(image,0,0,this);
+
+        if(isFocusOwner()){
+            g.setColor(Color.RED);
+        }else{
+            g.setColor(Color.BLACK);
+        }
+        g.drawRect(0,0,image.getWidth(this),image.getHeight(this));
+        g.dispose();
     }
 
     @Override
--- src/view/PicturePanel.java
+++ src/view/PicturePanel.java
@@ -1,14 +1,17 @@
 package view;
 
+import Elements.DTPicture;
 import model.ImageCollectionModel;
 import model.ImageModel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
 public class PicturePanel extends JPanel implements Observer {
+    private ArrayList<DTPicture> pic = new ArrayList<>();
     private ImageCollectionModel icm;
     public PicturePanel(ImageCollectionModel icm){
         this.icm=icm;
